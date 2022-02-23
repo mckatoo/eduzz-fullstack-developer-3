486dedc08801168fa27fa9435be61de723bb3257
Milton Carlos Katoo <mckatoo@gmail.com>
2022-02-23 11:36:26 -0300

    remove unnecessary properties

--- encurtador-url/frontend/src/providers/context.ts
+++ encurtador-url/frontend/src/providers/context.ts
@@ -2,24 +2,16 @@ import { createContext } from 'react'
 
 export type ShortenerContextProps = {
   loading: boolean
-  shortenerState: {
-    url: string
-    hash: string
-    shortURL: string
-  }
+  url: string
+  shortUrl: string
   shortenURL: (url: string) => Promise<void>
-  getURL: (url: string) => Promise<void>
 }
 
 const ShortenerContext = createContext<ShortenerContextProps>({
   loading: false,
-  shortenerState: {
-    url: '',
-    hash: '',
-    shortURL: ''
-  },
-  shortenURL: (url: string) => Promise.resolve(),
-  getURL: (hash: string) => Promise.resolve()
+  url: '',
+  shortUrl: '',
+  shortenURL: (url: string) => Promise.resolve()
 })
 
 export default ShortenerContext
--- encurtador-url/frontend/src/providers/shortner.tsx
+++ encurtador-url/frontend/src/providers/shortner.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import api from '../services/api'
 import ShortenerContext, { ShortenerContextProps } from './context'
@@ -10,48 +10,23 @@ type ShortenerProviderProps = {
 const ShortenerProvider = ({ children }: ShortenerProviderProps) => {
   const [shortener, setShortener] = useState({
     loading: false,
-    shortenerState: {
-      url: '',
-      hash: '',
-      shortURL: ''
-    }
+    shortUrl: '',
+    url: ''
   })
 
-  const getURL = async (hash: string) => {
-    setShortener((prevState) => ({
-      ...prevState,
-      loading: !prevState.loading,
-    }))
-
-    const { data } = await api.get(`/${hash}`)
-    setShortener((prevState) => ({
-      ...prevState,
-      url: data.url,
-      hash: data.hash,
-      shortURL: data.shortURL,
-      loading: !prevState.loading,
-    }))
-  }
-
   const shortenURL = async (url: string) => {
-    const { data: { hash, shortURL, url: _url } } = await api.post('/shorten', { url })
-
+    const { data: { shortUrl, url: _url } } = await api.post('/shorten', { url })
     setShortener((prevState) => ({
       ...prevState,
+      loading: !prevState.loading,
+      shortUrl,
       url: _url,
-      hash,
-      shortURL,
-      loading: !prevState.loading
     }))
   }
-
-  const { loading, shortenerState } = shortener
 
   const contextValues: ShortenerContextProps = {
-    loading,
-    shortenerState,
-    shortenURL: (url: string) => shortenURL(url),
-    getURL: (hash: string) => getURL(hash)
+    ...shortener,
+    shortenURL: useCallback((url: string) => shortenURL(url), []),
   }
 
   return (
