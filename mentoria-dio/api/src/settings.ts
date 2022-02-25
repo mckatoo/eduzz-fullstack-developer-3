e046c0860fdd3ba2f05ff83a97a5f0b2cb1476e9
Milton Carlos Katoo <mckatoo@gmail.com>
2022-02-25 11:23:10 -0300

    add mentoria-eduzz

---
+++ mentoria-dio/api/src/settings.ts
@@ -0,0 +1,9 @@
+export const IS_DEVELOPMENT = true;
+
+export const RABBIT_HOST= 'amqp://localhost';
+export const RABBIT_USER='';
+export const RABBIT_PASSWORD='';
+export const RABBIT_PORT=5672;
+export const RABBIT_DSN = IS_DEVELOPMENT ? `${RABBIT_HOST}` : ``;
+
+export const BASE_URL_FRONT = 'http://localhost:3000';
