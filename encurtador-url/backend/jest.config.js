61586a3b9a7f40451fb31fbfcbc3ef04c9bb7242
Milton Carlos Katoo <mckatoo@gmail.com>
2022-07-02 13:11:17 -0300

    Test setup with supertest

---
+++ encurtador-url/backend/jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  coverageProvider: "v8",
+  transform: {
+    "^.+\\.ts?$": ["@swc/jest"],
+  },
+};
